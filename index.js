c4ab215c3b5d6666c515ba8cee94ee22a3811ec7
Pavlinka100 <164795472+Pavlinka100@users.noreply.github.com>
2024-09-05 20:43:22 +0200

    finished layout and fixed speed bug

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 
 function startGame(event) {
-    console.log(event);
-    console.log("startGame, removing click listnere, changing to listen");    
-    //gameControlGif.removeEventListener("click", startGame); // remove first click event listener
- 
+    //let disappear options as the game is started
     speedOptions.classList.add("d-none");
     startStopButton.classList.remove("d-none");
+    rules.classList.add("d-none");
+
     stopped = false;
     startStopButton.textContent = "Stop"; 
+
     speedLevel = document.querySelector(".amgamespeed").value;
     fullDelay = Math.floor(delay/speedLevel);
-    console.log("speed level");
-    console.log(speedLevel);
-    console.log(fullDelay);
+
     playGame(n); 
-    console.log("play game started");   
-    rules.classList.add("d-none");
+   
 }
 
 function stopGame(event){
-    stopped = true; 
-    console.log("inside stopGame");  
-    gameControlGif.src = buttonImage;
-    document.removeEventListener("keydown", catchPressedKey);
-    gameControlGif.removeEventListener("click", catchPressedKey);
-    speedOptions.classList.remove("d-none");
-    console.log("tady ma by stopped");
-    startStopButton.textContent = "Restart"; 
-    rules.classList.remove("d-none");
+    location.reload();
 }
 
 
@@ -37,11 +26,11 @@ function playRandomSound(){
     n = Math.floor(n);
     var audio = new Audio("sounds/" + AudioNamesArray[n]);
     audio.play();
-    console.log("sounds/" + AudioNamesArray[n])
+
 }
 
 function catchPressedKey(event) {
-    console.log(event);
+
     gameControlGif.src = "gifs/" + gifsNamesInitialArray[(n+round) % gifsNamesInitialArray.length];
     round++;
     document.removeEventListener("keydown", catchPressedKey);
@@ -58,13 +47,10 @@ function catchPressedKey(event) {
 function playGame(){
     
     gameControlGif.src = buttonImage;
-
     randomMiliSeconds = Math.random()*1000 + fullDelay;
 
-    setTimeout(function(event) {    
-        console.log("set timeout to catch keydowns"); 
-        console.log();
-        
+
+    setTimeout(function(event) {          
         document.addEventListener("keydown", catchPressedKey);
         gameControlGif.addEventListener("click", catchPressedKey);
         if (stopped==false){playRandomSound();};
@@ -75,20 +61,19 @@ function playGame(){
             
             if (stopped==false){playGame(); };
 
-        },fullDelay);
+        },randomMiliSeconds);
+        
        
-        }, randomMiliSeconds); 
+        }, 5000); 
     
     
 }
 
 
-
-
 //Initial setup of the game
 
 //setup of the sounds and images
-//should be replaced by getting the folder from server side, for now leaving as it is as I should be able to host it on github
+//should be replaced by getting the folder from server side, based on github hosting, leaving as it is
 var gifsNamesInitialArray = [ "bell-pepper-8079_256.gif", "cartoon-571_256.gif", "character-13952_256.gif", "dog-3343_256.gif",
     "flower-11997_256.gif", "flying-7288_256.gif", "ladybug-5068_256.gif", "pinwheel-8829_256.gif",
     "star-19_256.gif", "valentine-3652_256.gif","wall-8423_256.gif" ]
@@ -97,21 +82,16 @@ var AudioNamesArray =["100.mp3", "1500.mp3", "200.mp3","300.mp3", "500.mp3","800
 var speedLevel = 1;
 var delay = 20000; //number of ms to delay SetTimout
 var fullDelay = delay;
-console.log("fulldelaz");
-console.log(fullDelay);
 
 var gameControlGif = document.querySelector(".amgamegif");
-
-
 var startStopButton = document.querySelector(".amstartstopbutton");
-
-
-
 var speedOptions = document.querySelector(".amgamespeedoptions");
+
+
 var stopped = true;
 var buttonImage ="images/button-162066_640.png";
 var rules = document.querySelector(".pravidla");
-//gameControlGif.addEventListener("click", startGame); 
+
 
 //getting random number to start with the gifs in the array as start is randomly picked up and affer it one by one is shown
 var n = Math.floor(Math.random() * gifsNamesInitialArray.length);
