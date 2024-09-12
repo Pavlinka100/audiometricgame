50d87ff22a09002f13954432c34595ac077be3a6
Pavlinka100 <164795472+Pavlinka100@users.noreply.github.com>
2024-09-12 16:26:58 +0200

    stopped debugging

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var gifsNames = ["avocado-1113_512.gif", "bell-pepper-8079_256.gif", "butterfly-
     "sheep-6470_256.gif", "star-19_256.gif","valentine-3652_256.gif", "wall-8423_256.gif",
     "whale-155_512.gif"];
 
-var AudioNames =[ "1500.mp3", "200.mp3","300.mp3", "500.mp3","800.mp3","1000.mp3"]; 
+var AudioNames =["200.mp3","300.mp3", "500.mp3","800.mp3","1000.mp3"]; 
 
 var listening = false;
 var soundCatched = false;
@@ -154,7 +154,7 @@ function playGame(){
 function playRandomSound(){
     let i = Math.floor(Math.random() * AudioNames.length);
     audio = new Audio("sounds/" + AudioNames[i]);
-    $(".amgstartstop").text(AudioNames[i]);
+    
     audio.play();
     //setTimeout(function(){audio.pause();},2700); //the audio is 3 sec long, can be adjusted, later maybe included in speed options
     
